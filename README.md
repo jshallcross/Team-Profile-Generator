89e17f2efdd5864a750e1706cd22063f7697e8ba
Jon Shallcross <jshallcross1@icloud.com>
2021-02-13 18:24:22 -0500

    Comments and ReadMe created. Validation for all questions complete.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Team-Profile-Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Grading Requirements
+---
+
+
+## Description
+---   
+User input is turned into a team profile website. All fields are required to be entered. Additionally the email must be a valid form.
+
+## Installation
+---  
+Install node. Run by typing index.js in the terminal.
+
+## Usage
+---  
+Easily create a profile of your work team to access their information. Created file found in the dist folder.
+
+## Contributions
+---
+Stack overflow helped sort out the RegExp email validator. This
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const makeHtml = require("./lib/htmlTemplate");
 const fs = require("fs");
 const path = require("path");
-const makeHtml = require("./lib/htmlTemplate");
 
-const outputFolder = path.resolve(__dirname, "output")
+
+const outputFolder = path.resolve(__dirname, "dist")
 const outputFile = path.join(outputFolder, "team.html");
 
 
@@ -25,28 +26,58 @@ class Team {
             
         }    
     }
-    
+    // Function to create a Manager
     makeManager() {
+        console.log("Please build your team below. All fields are required.")
         inquirer
             .prompt([{
                 type: 'text',
                 name: 'name',
-                message: "What is your Manager's name?"
+                message: "What is your Manager's name?",
+                validate: nameInput => {
+                    if(nameInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Manager's name!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'text',
                 name: 'idNumber',
-                message: "What is your Manager's employee ID number?"
+                message: "What is your Manager's employee ID number?",
+                validate: idInput => {
+                    if(idInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Manager's employee number!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'text',
                 name: "email",
-                message: "What is your Manager's email address?"
+                message: "What is your Manager's email address?",
+                validate: function(email)
+                {
+                // Regex mail check (return true if valid mail)
+                return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                }
             },
             {    
                 type: 'text',
                 name: 'officeNumber',
-                message: "What is your Manager's office number?"
+                message: "What is your Manager's office number? Format '905-555-5555'",
+                validate: officeNumberInput => {
+                    if(officeNumberInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Manager's office number!");
+                        return false;
+                    }
+                }
             }
             ]).then(answers => {
                 const manager = new Manager(answers.name, answers.idNumber, answers.email, answers.officeNumber);
@@ -55,7 +86,7 @@ class Team {
                 this.addTeamMember();
         })
     }
-
+    // Function to add a team member based on a choice
     addTeamMember() {
         inquirer
             .prompt([{
@@ -70,34 +101,63 @@ class Team {
             } else if (answer.choice === 'Intern'){
                 this.makeIntern();
             } else if (answer.choice === 'None'){
-                //make html
+                // write file 
                 this.display();
             } else {
                 this.addTeamMember();
             }     
         })
     }
+    // Function to create an Engineer
     makeEngineer() {
         inquirer
             .prompt([{
                 type: 'text',
                 name: 'name',
-                message: "What is your Engineer's name?"
+                message: "What is your Engineer's name?",
+                validate: nameInput => {
+                    if(nameInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Engineer's name!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'text',
                 name: 'idNumber',
-                message: "What is your Engineer's employee ID number?"
+                message: "What is your Engineer's employee ID number?",
+                validate: idInput => {
+                    if(idInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Engineer's employee number!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'text',
                 name: "email",
-                message: "What is your Engineer's email address?"
+                message: "What is your Engineer's email address?",validate: function(email)
+                {
+                    // Regex mail check (return true if valid mail)
+                    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                }
             },
             {    
                 type: 'text',
                 name: 'github',
-                message: "What is your Engineer's GitHub username?"
+                message: "What is your Engineer's GitHub username?",
+                validate: githubInput => {
+                    if(githubInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Engineer's GitHub username!");
+                        return false;
+                    }
+                }
             }
             ]).then(answers => {
                 const engineer = new Engineer(answers.name, answers.idNumber, answers.email, answers.github);
@@ -106,27 +166,57 @@ class Team {
                 this.addTeamMember();
         })
     }
+    // Function to create an Intern
     makeIntern() {
         inquirer
             .prompt([{
                 type: 'text',
                 name: 'name',
-                message: "What is your Intern's name?"
+                message: "What is your Intern's name?",
+                validate: nameInput => {
+                    if(nameInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Intern's name!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'text',
                 name: 'idNumber',
-                message: "What is your Intern's employee ID number?"
+                message: "What is your Intern's employee ID number?",
+                validate: idInput => {
+                    if(idInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Intern's employee number!");
+                        return false;
+                    }
+                }
             },
             {
                 type: 'text',
                 name: "email",
-                message: "What is your Intern's email address?"
+                message: "What is your Intern's email address?",
+                validate: function(email)
+                {
+                // Regex mail check (return true if valid mail)
+                return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                }
             },
             {    
                 type: 'text',
                 name: 'school',
-                message: "What is your Intern's school?"
+                message: "What is your Intern's school?",
+                validate: schoolInput => {
+                    if(schoolInput){
+                        return true;
+                    } else{
+                        console.log("Please enter your Intern's School!");
+                        return false;
+                    }
+                }
             }
             ]).then(answers => {
                 const intern = new Intern(answers.name, answers.idNumber, answers.email, answers.school);
@@ -136,7 +226,7 @@ class Team {
                 
         })
     }
-
+    // write the new file with the newly created team
     display() {
         fs.writeFile(outputFile, makeHtml(this.team), function (err){
             if (err) {
@@ -148,7 +238,6 @@ class Team {
     }
 
 }
-
 
 let team = new Team();
 team.newTeam();
